06449244ef6020e23c07a1d377349d1155996e92
Max Giesbers <max_giesbers@hotmail.com>
2019-02-22 15:50:15 +0100

    change 1

--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ int main ()
     std::cout << " dit is een change" << std::endl;
     std::cout << "lijntje geklapt" << std::endl;
     std::cout << " test" << std::endl;
+    change 1
     return 0;
 }
